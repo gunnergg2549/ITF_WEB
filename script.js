2d2731c391ae076c8bf94c4089196fb4557179f3
gunnergg2549 <gunkritsada.pr@gmail.com>
2025-10-06 16:10:35 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-const myinput = document.getElementById("myinput")
+function addHistory(action, amount, status="") {
+    let history = document.getElementById("history");
+    let time = new Date().toLocaleTimeString();
+    history.value += `[${time}] ${action} : ${amount} ${status}\n`;
+}
 
-function add(){
-    myinput.value = Number(myinput.value) + 1
+function deposit() {
+    let amount = parseInt(document.getElementById("amount").value);
+    if(amount > 0){
+        let acc = document.getElementById("inputAccount");
+        let cash = document.getElementById("inputCash");
+
+        acc.value = parseInt(acc.value) + amount;
+        cash.value = parseInt(cash.value) - amount;
+
+        addHistory("Deposit", amount, "✅");
+    } else {
+        addHistory("Deposit", amount, "❌ Invalid amount");
+    }
+}
+
+function withdraw() {
+    let amount = parseInt(document.getElementById("amount").value);
+    let acc = document.getElementById("inputAccount");
+    let cash = document.getElementById("inputCash");
+
+    if(amount > 0 && amount <= parseInt(acc.value)){
+        acc.value = parseInt(acc.value) - amount;
+        cash.value = parseInt(cash.value) + amount;
+
+        addHistory("Withdraw", amount, "✅");
+    } else {
+        addHistory("Withdraw", amount, "❌ Not enough balance");
+    }
+}
+
+function currenct_change(){
+    
 }
